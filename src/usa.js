b4807f458651a0ada7933e1387c871348c1e42a1
LannyWang009 <09legend.wang@gmail.com>
2019-03-26 00:06:04 -0500

    added color gradient based on total trade activity

--- src/usa.js
+++ src/usa.js
@@ -1,31 +1,73 @@
-var width = 960
-var height = 500
+/* This visualization was made possible by modifying code provided by:
+Scott Murray, Choropleth example from "Interactive Data Visualization for the Web"
+https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html
+
+Nick Zhu, D3 Cookbook2, Chapter 12
+https://github.com/NickQiZhu/d3-cookbook/blob/master/src/chapter12/usa.html
+
+Michelle Chandra's "Basic US State Map -D3"
+http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
+*/
+
+// define the size of the map
+var width = 960 * 0.7
+var height = 500 * 0.95
 var path = d3.geoPath() // <- A
-  .projection(d3.geoAlbersUsa().scale([900]))
-var svg = d3.select('#map').append('svg')
+  .projection(d3.geoAlbersUsa().translate([width / 2, height / 2]).scale([800]))
+var svg = d3.select('#themap').append('svg')
   .attr('width', width)
   .attr('height', height)
+
 var g = svg.append('g')
-//   .call(d3.zoom()
-//     .scaleExtent([1, 10])
-//     .on('zoom', zoomHandler))
-d3.json('../../data/json/us.json', function (error, us) {
+
+// Define linear scale for color output
+var mapColor = d3.scaleQuantize()
+  .range(['#c7c7ce', '#c5bfce', '#c2b7cd', '#beadcd', '#baa3cc', '#b597cb', '#a97dc9', '#995cc6', '#8a3fc3', '#812cc1'])
+
+d3.csv('../../data/csv/allState2018.csv', function (error, data) {
   if (error) { console.log('error', error) }
-  g.insert('path') // <-B
-    .datum(topojson.feature(us, us.objects.land))
-    .attr('class', 'land')
-    .attr('d', path)
-  g.selectAll('path.state')
-    .data(topojson.feature(us,
-      us.objects.states).features) // <-C
-    .enter()
-    .append('path')
-    .attr('class', 'state')
-    .attr('d', path)
-})
-// function zoomHandler () {
-//   var transform = d3.event.transform
-//   g.attr('transform', 'translate(' +
-//     transform.x + ',' + transform.y +
-//     ')scale(' + transform.k + ')')
-// }
+  console.log('data', data)
+  mapColor.domain([
+    d3.min(data, function (d) { return d.total_trade_activity }),
+    d3.max(data, function (d) { return d.total_trade_activity })
+  ])
+
+  // load GeoJSON data and merge with allstate2018 data
+  d3.json('../../data/json/us-states.json', function (error, json) {
+    if (error) { console.log('error', error) }
+    console.log('usjson', json)
+    for (var i = 0; i < data.length; i++) {
+      // grab te state name and total trade activity value in billions
+      var dataState = data[i].state
+      var dataValue = data[i].total_trade_activity
+
+      // Find the corresponding state inside the GeoJSON
+      for (var j = 0; j < json.features.length; j++) {
+        var jsonState = json.features[j].properties.name
+        if (dataState == jsonState) {
+          // copy the data value into the json
+          json.features[j].properties.value = dataValue
+          // stop looking through the json
+          break
+        }
+      }
+    }
+
+    // Bind the data to the SVG and create one ath per GeoJSON feature
+    svg.selectAll('path')
+      .data(json.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .style('stroke', 'grey')
+      .style('fill', function (d) {
+        let value = d.properties.value
+        if (value) {
+          return mapColor(value)
+        } else {
+          return 'grey'
+        }
+      })
+  })
+}
+)
