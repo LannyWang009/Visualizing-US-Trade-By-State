bb22d982319fe20bc7d92f87fafe43908b10d895
LannyWang009 <09legend.wang@gmail.com>
2019-03-26 18:53:22 -0500

    to save

--- src/barChart_tradevolume.js
+++ src/barChart_tradevolume.js
@@ -4,19 +4,27 @@ d3.csv('./data/csv/allState2018.csv', function (error, csvdata) {
   console.log('datasetTradeVolume', csvdata)
   datasetTradeVolume = csvdata
   // create scale
-  var s = 550
+  var s = 400
+  var h = 570
   const range = [0, s]
   const domain = [0, 770]
-  var linearscale = d3.scaleLinear()
+  var xlinearscale = d3.scaleLinear()
     .domain(domain)
     .range(range)
 
-  d3.select('#importbar').selectAll('rect')
+  const svg = d3.select()
+
+  d3.select('#exportbar')
+    .append('g')
+    .attr('fill', '#ebe4eb')
+    .selectAll('rect')
     .data(datasetTradeVolume)
     .enter()
-    .append('rect')
-    .attr('x')
-    .attr('y')
+    .join('rect')
+    .attr('x', xlinearscale(450))
+    .attr('y', x)
+    .width('width', d => xlinearscale(d.export_2018))
+    .height('height', d => h / d.length)
 })
 
 // function conversor (d) {
