7625246fd731f02aa7796f90631ce94ca219103a
LannyWang009 <09legend.wang@gmail.com>
2019-03-26 01:51:31 -0500

    creating the export and import bar charts

--- src/barChart_tradevolume.js
+++ src/barChart_tradevolume.js
@@ -2,6 +2,21 @@ var datasetTradeVolume
 d3.csv('./data/csv/allState2018.csv', function (error, csvdata) {
   if (error) { console.log(error) }
   console.log('datasetTradeVolume', csvdata)
+  datasetTradeVolume = csvdata
+  // create scale
+  var s = 550
+  const range = [0, s]
+  const domain = [0, 770]
+  var linearscale = d3.scaleLinear()
+    .domain(domain)
+    .range(range)
+
+  d3.select('#importbar').selectAll('rect')
+    .data(datasetTradeVolume)
+    .enter()
+    .append('rect')
+    .attr('x')
+    .attr('y')
 })
 
 // function conversor (d) {
