5fd485ee48434fec4a918475fbf2aaeca446c699
LannyWang009 <09legend.wang@gmail.com>
2019-03-30 02:44:42 -0500

    format

--- src/country-export.js
+++ src/country-export.js
@@ -2,118 +2,194 @@
 let exportBarChartData
 
 const exportColors = [
-    "#9221ac",
-    "#d5c3fc",
-    "#a085de",
-    "#4d308b",
-    "#714fdb"
+  '#9221ac',
+  '#d5c3fc',
+  '#a085de',
+  '#4d308b',
+  '#714fdb'
 ]
 
-function renderExportGraph() {
-    d3.csv("./data/csv/ExportByCountry.csv", numConverter, exportGraph)
+function renderExportGraph () {
+  d3.csv('./data/csv/ExportByCountry.csv', numConverter, exportGraph)
 
-    function numConverter(d) {
-        d.Exports = parseFloat(d.Exports.replace(/,/g, ''));
-        d.Time = +d.Time;
-        return d;
-    }
+  function numConverter (d) {
+    d.Exports = parseFloat(d.Exports.replace(/,/g, ''))
+    d.Time = +d.Time
+    return d
+  }
 }
 
 renderExportGraph()
 
-function exportGraph(error, data) {
-
-    //Filter dataset
-    if(error) {
-        console.log('Error occurred while loading data:', error)
-    } else {
-        let statesData = []
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].State === "Texas" && data[i].Time === 2018 && data[i].Country != "World Total") {
-                statesData.push(data[i])
-            }
-        }
-
-        statesData.sort(function (a, b) {
-            return b.Exports - a.Exports
-        })
-
-        exportBarChartData = statesData.slice(0, 10)
-
+function exportGraph (error, data) {
+  // Filter dataset
+  if (error) {
+    console.log('Error occurred while loading data:', error)
+  } else {
+    let statesData = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].State === 'Texas' && data[i].Time === 2018 && data[i].Country != 'World Total') {
+        statesData.push(data[i])
+      }
     }
 
-    console.log(exportBarChartData, exportBarChartData.length)
+    statesData.sort(function (a, b) {
+      return b.Exports - a.Exports
+    })
 
-    //Build Export Bar Chart Graph
-    const w = 400;
-    const h = 225;
-    const padding = 30
+    exportBarChartData = statesData.slice(0, 10)
+  }
 
-    //Build tooltip
-    let barTooltip = d3.select("#exportBarChart")
-                        .append("div")
-                        .attr("class", "tooltip")
-                        .style("opacity", 0)
+  console.log(exportBarChartData, exportBarChartData.length)
 
-    //Define scales
-    const xScale = d3.scaleLinear()
-                    .domain([0, d3.max(exportBarChartData, (d) => d.Exports)])
-                    .range([padding, w]);
- 
-    //Add Chart svg
-    const svg = d3.select("#exportBarChart")
-                    .append("svg")
-                    .attr("width", w)
-                    .attr("height", h)
-                    .attr("class", "exportsChart");
+  // Build Export Bar Chart Graph
+  const w = 400
+  const h = 225
+  const padding = 30
 
-    //Build bars
-    svg.selectAll("rect")
-        .data(exportBarChartData)
-        .enter()
-        .append("rect")
-        .attr("x", padding)
-        .attr("y", (d, i) => i * (h / exportBarChartData.length))
-        .attr("height", (d) => (h / exportBarChartData.length) - 5)
-        .attr("width", (d) => xScale(d.Exports))
-        .attr("fill", "#9B88D8")
-        .attr("class", "chartBar")
-        .on("mouseover", function(d) {
+  // Build tooltip
+  let barTooltip = d3.select('#exportBarChart')
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0)
 
-            barTooltip.transition()
-                .duration(500)
-                .style("opacity", .9)
+  // Define scales
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(exportBarChartData, (d) => d.Exports)])
+    .range([padding, w])
 
-            format = d3.format(",")
+  // Add Chart svg
+  const svg = d3.select('#exportBarChart')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .attr('class', 'exportsChart')
 
-            var tip = "<strong>Total Exports to " + d.Country + ":</strong> $" + format(d.Exports)
-            
-            barTooltip.html(tip)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
+  // Build bars
+  svg.selectAll('rect')
+    .data(exportBarChartData)
+    .enter()
+    .append('rect')
+    .attr('x', padding)
+    .attr('y', (d, i) => i * (h / exportBarChartData.length))
+    .attr('height', (d) => (h / exportBarChartData.length) - 5)
+    .attr('width', (d) => xScale(d.Exports))
+    .attr('fill', '#9B88D8')
+    .attr('class', 'chartBar')
+    .on('mouseover', function (d) {
+      barTooltip.transition()
+        .duration(500)
+        .style('opacity', 0.9)
 
-            barTooltip.transition()
-                .duration(500)
-                .style("opacity", 0)
-        })
+      format = d3.format(',')
 
-        svg.selectAll("text")
-        .data(exportBarChartData)
-        .enter()
-        .append("text")
-        .text(function(d, i) {
-                return exportBarChartData[i].Country;
-        })
-        .attr("text-anchor", "left")
-        .attr("y", (d, i) => 11 + i * (h / exportBarChartData.length))
-        .attr("x", padding + 5)
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", "white")
+      var tip = '<strong>Total Exports to ' + d.Country + ':</strong> $' + format(d.Exports)
 
+      barTooltip.html(tip)
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY - 28) + 'px')
+    })
+    .on('mouseout', function (d) {
+      barTooltip.transition()
+        .duration(500)
+        .style('opacity', 0)
+    })
+
+  svg.selectAll('text')
+    .data(exportBarChartData)
+    .enter()
+    .append('text')
+    .text(function (d, i) {
+      return exportBarChartData[i].Country
+    })
+    .attr('text-anchor', 'left')
+    .attr('y', (d, i) => 11 + i * (h / exportBarChartData.length))
+    .attr('x', padding + 5)
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '11px')
+    .attr('fill', 'white')
 }
 
+// Update Export Bar Chart
+function updateExportGraph () {
+  d3.csv('./data/csv/ExportByCountry.csv', numConverter, updatedExportGraph)
 
+  function numConverter (d) {
+    d.Exports = parseFloat(d.Exports.replace(/,/g, ''))
+    d.Time = +d.Time
+    return d
+  }
+}
 
+function updatedExportGraph (error, data) {
+  // Filter dataset
+  if (error) {
+    console.log('Error occurred while loading data:', error)
+  } else {
+    let statesData = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].State === selectedState && data[i].Time === 2018 && data[i].Country != 'World Total') {
+        statesData.push(data[i])
+      }
+    }
+
+    statesData.sort(function (a, b) {
+      return b.Exports - a.Exports
+    })
+
+    exportBarChartData = statesData.slice(0, 10)
+  }
+
+  // console.log(exportBarChartData, exportBarChartData.length)
+
+  const w = 400
+  const h = 225
+  const padding = 30
+
+  // Update tooltip
+  let barTooltip = d3.select('#exportBarChart')
+
+  barTooltip.exit().remove()
+
+  barTooltip.enter()
+    .append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0)
+
+  // Define scales
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(exportBarChartData, (d) => d.Exports)])
+    .range([padding, w])
+
+  // Update chart
+  const svg = d3.select('#exportBarChart')
+
+  // Update bars
+  svg.selectAll('rect')
+    .data(exportBarChartData)
+    .transition().duration(1000)
+    .attr('x', padding)
+    .attr('y', (d, i) => i * (h / exportBarChartData.length))
+    .attr('height', (d) => (h / exportBarChartData.length) - 5)
+    .attr('width', (d) => xScale(d.Exports))
+    .attr('fill', '#9B88D8')
+    .attr('class', 'chartBar')
+
+  // Update labels
+  svg.selectAll('text')
+    .data(exportBarChartData)
+    .transition()
+    .delay(function (d, i) {
+      return i * 50
+    })
+    .duration(2000)
+    .text(function (d, i) {
+      return exportBarChartData[i].Country
+    })
+    .attr('text-anchor', 'left')
+    .attr('y', (d, i) => 11 + i * (h / exportBarChartData.length))
+    .attr('x', padding + 5)
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '11px')
+    .attr('fill', 'white')
+}
--- src/yearSelection.js
+++ src/yearSelection.js
@@ -1,13 +1,5 @@
 var slider = document.getElementById('myRange')
 var selectedTime
-// var output = document.getElementById('demo')
-
-// output.innerHTML = slider.value // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function () {
-//   output.innerHTML = this.value
-// }
 
 d3.select('input')
   .on('change', function () {
@@ -21,3 +13,107 @@ d3.select('input')
     updateExportPack()
     updateImportPack()
   })
+
+// ============All about assignming Colors==================
+var expColors =
+['#ae3871',
+  '#3e40d3',
+  '#df6fa3',
+  '#4e1ab2',
+  '#e53296',
+  '#5f8ddb',
+  '#7a28d1',
+  '#7a2255',
+  '#9251f2',
+  '#752063',
+  '#4d62ea',
+  '#b93497',
+  '#6a7ee8',
+  '#e04eca',
+  '#445ba7',
+  '#d851ec',
+  '#313380',
+  '#e78bd7',
+  '#37149a',
+  '#b390e0',
+  '#48218e',
+  '#bb6fce',
+  '#502a69',
+  '#aa3dcf',
+  '#7a53a0',
+  '#7231b8',
+  '#a34e97',
+  '#574db8',
+  '#682278',
+  '#a16de7',
+  '#9131a7']
+
+// var expColors =
+// ['#7fc5c9',
+//   '#ecb1c2',
+//   '#b2e5c2',
+//   '#7fcfe0',
+//   '#d6eec0',
+//   '#ffcbbd',
+//   '#abc89b',
+//   '#dabbe9',
+//   '#a2ece5',
+//   '#ccd9a6',
+//   '#c1bced',
+//   '#cac092',
+//   '#a6c3ed',
+//   '#e9bc99',
+//   '#87c9ed',
+//   '#eeebc0',
+//   '#bbe4ee',
+//   '#d0b89c',
+//   '#bad6d0',
+//   '#e2a997',
+//   '#94c5b3',
+//   '#ddb2b6',
+//   '#b5e6bb',
+//   '#e1d9ee',
+//   '#bbc5aa',
+//   '#c1b3d0',
+//   '#e7ddc9',
+//   '#acbed0',
+//   '#edd3d5',
+//   '#c9b6b0',
+//   '#9131a7']
+
+function switchColor (commodity) {
+  switch (commodity) {
+    case '111 Agricultural Products':return '#9221ac'
+    case '112 Livestock & Livestock Products':return '#d5c3fc'
+    case '113 Forestry Products, Nesoi':return expColors[2]
+    case '114 Fish, Fresh/chilled/frozen & Other Marine Products': return expColors[3]
+    case '211 Oil & Gas': return '#714fdb'
+    case '212 Minerals & Ores': return expColors[5]
+    case '311 Food & Kindred Products': return expColors[6]
+    case '312 Beverages & Tobacco Products': return '#a085de'
+    case '313 Textiles & Fabrics': return expColors[8]
+    case '314 Textile Mill Products': return expColors[9]
+    case '315 Apparel & Accessories': return expColors[10]
+    case '316 Leather & Allied Products': return expColors[11]
+    case '321 Wood Products': return expColors[12]
+    case '322 Paper': return expColors[13]
+    case '323 Printed Matter And Related Products, Nesoi': return expColors[14]
+    case '324 Petroleum & Coal Products': return expColors[15]
+    case '325 Chemicals': return '#4d308b'
+    case '326 Plastics & Rubber Products': return expColors[17]
+    case '327 Nonmetallic Mineral Products': return expColors[18]
+    case '331 Primary Metal Mfg': return expColors[19]
+    case '332 Fabricated Metal Products, Nesoi': return expColors[20]
+    case '333 Machinery, Except Electrical': return expColors[21]
+    case '334 Computer & Electronic Products': return expColors[22]
+    case '335 Electrical Equipment, Appliances & Components': return expColors[23]
+    case '336 Transportation Equipment': return expColors[24]
+    case '337 Furniture & Fixtures': return expColors[25]
+    case '339 Miscellaneous Manufactured Commodities': return expColors[26]
+    case '910 Waste And Scrap': return expColors[27]
+    case '930 Used Or Second-hand Merchandise': return expColors[28]
+    case '980 Goods Returned (exports For Canada Only)': return expColors[29]
+    case '990 Other Special Classification Provisions': return expColors[30]
+    default:return 'rgba(0, 0, 0, 0)'
+  }
+}
