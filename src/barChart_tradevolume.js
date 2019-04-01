f0e5dcccca8d47afa13eec5fbfcc4d554cef29ff
Becca Randall <becca.m.randall@gmail.com>
2019-03-31 20:00:36 -0500

    Added lollipop barchart

--- src/barChart_tradevolume.js
+++ src/barChart_tradevolume.js
@@ -1,111 +1,214 @@
 var tradeVolumeDataset
+var stateNames
 
-d3.csv('./data/csv/allState2018.csv', function (error, csvdata) {
+d3.csv('./data/csv/allState2018.csv', numConverter, function (error, csvdata) {
   
   // Sort Data
   if (error) { 
     console.log('There was an error loading trade volume data: ', error) 
-  } else {
-    console.log('csvdata = ', csvdata)
-    var dataset = d3.layout.stack()(["import_2018", "export_2018"].map(function(trade) {
-      return csvdata.map(function(d) {
-        return {x: parseInt(d.total_trade_activity), y: +d[trade]}
-      })
-    })
-    )
-    console.log('the new dataset = ', dataset)
-    tradeVolumeDataset = dataset
-
-    }
+  } 
+  console.log(csvdata)
+  tradeVolumeDataset = csvdata
   
   // Setup chart 
-  var margin = {top: 50, right: 10, bottom: 0, left: 10}
+  var margin = {top: 50, right: 10, bottom: 0, left: 105}
   var width = 432 - margin.right - margin.left
   var height = 577 - margin.top - margin.bottom
+  var colors = ['#6b55d3', 'pink']
 
   var svg = d3.select('#bar')
-              .append('svg')
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-  var tooltip = d3.select('#bar')
-    .append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 0)
-
-  // Set up scale
-  var yScale = d3.scale.ordinal()
-            .domain(tradeVolumeDataset[0].map(function(d) { return d.x; }))
-            .rangeRoundBands([10, height-10], 0.05)
-
-  var xScale = d3.scale.linear()
-            .domain([0, d3.max(tradeVolumeDataset, function(d) {
-                          return d3.max(d, function(d) { return d.y0 + d.y;
-                        })
-                      })
-                    ])  
-            .range([0, width])
+                .append('svg')
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-  var colors = ['#6b55d3', '#cf5cc5']
+  // Axes
+  var xScale = d3.scaleLinear()
+                .domain([0, 620.1])
+                .range([0, width])
 
-  // // Axis
-  // var yAxis = d3.svg.axis()
-  //               .scale(yScale)
-  //               .orient('left')
+  var yScale = d3.scaleBand()
+                .range([0, height])
+                .domain(tradeVolumeDataset.map(function(d) { return d.state}))
+                .padding(1)
+                
+  svg.append('g')
+      .attr('class', 'axisLabel')
+      .call(d3.axisLeft(yScale))
+      .selectAll('text')
+      .data(tradeVolumeDataset)
+      .enter()
+      .append('text')
+      .text(function(d) { return d.state})
+      .attr('fill', 'white')
+      .attr('class', 'axisLabels')
+  
+  // Lines
+  svg.selectAll('lines')
+      .data(tradeVolumeDataset)
+      .enter()
+      .append('line')
+      .attr('x1', function(d) { return xScale(d.total_trade_activity)})
+      .attr('x2', xScale(0))
+      .attr('y1', function(d) { return yScale(d.state)})
+      .attr('y2', function(d) { return yScale(d.state)})
+      .attr('stroke', 'white')
 
-  // svg.append('g')
-  //     .attr('class', 'y axis')
-  //     .call(yAxis)   
+  // Circles
+  svg.selectAll('import-circles')
+      .data(tradeVolumeDataset)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) { return xScale((d.import_2018 + d.export_2018))})
+      .attr('cy', function(d) { return yScale(d.state)})
+      .attr('r', '4')
+      .style('fill', colors[0])
+      .attr('stroke', colors[0])
 
-  // Create groups for each series, rects for each segment
-  var groups = svg.selectAll("g")
+  svg.selectAll('export-circles')
+      .data(tradeVolumeDataset)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) { return xScale((d.export_2018))})
+      .attr('cy', function(d) { return yScale(d.state)})
+      .attr('r', '4')
+      .style('fill', function(d) {return colors[1]})
+      .attr('stroke', function(d) {return colors[1]})
+
+  var legend = svg.selectAll('.legend')
                   .data(tradeVolumeDataset)
                   .enter()
                   .append('g')
-                  .attr("class", "volume")
-                  .style("fill", function(d,i) { return colors[i]})
-          
-  var rect = groups.selectAll("rect")
-                    .data(function(d) {return d})
-                    .enter()
-                    .append('rect')
-                    .attr("y", function(d) {return yScale(d.x)})
-                    .attr("x", function(d) { return xScale(d.y0)})
-                    .attr("height", yScale.rangeBand())
-                    .attr("width", function(d) { return xScale(d.y0 + d.y) - xScale(d.y0)})
-                    .on('mouseover', function (d) {
-                      tooltip.transition()
-                        .duration(500)
-                        .style('opacity', 0.9)
-                
-                      format = d3.format(',')
-                
-                      var tip = '$' + d.y + ' B'
-                
-                      tooltip.html(tip)
-                        .style('left', (d3.event.pageX) + 'px')
-                        .style('top', (d3.event.pageY - 28) + 'px')
-                    })
-                    .on('mouseout', function (d) {
-                      tooltip.transition()
-                        .duration(500)
-                        .style('opacity', 0)
-                    })
+                  .attr('class', 'legend')
+                  .attr('transform', function(d, i) {return 'translate (30,' + i * 19 + ')'})
+    
+  legend.append('rect')
+        .attr('x', width - 18)
+        .attr('width', 18)
+        .attr('height', 18)
+        .style('fill', function (d, i) {return colors[i]})
 
-  // var labels = svg.selectAll('text')
-  //                 .data(csvdata)
-  //                 .enter()
-  //                 .append('text')
-  //                 .text(function (d, i) {
-  //                   return csvdata[i].state
-  //                 })
-  //                 .attr('text-anchor', 'left')
-  //                 .attr('y', (d, i) => 26 + i * 12)
-  //                 .attr('x', 0)
-  //                 .attr('fill', 'white')
-  //                 .style('font-size', '8px')
-  //                 .style('font-family', 'sans-serif')
-          
 })
+
+function numConverter (d) {
+  d.total_trade_activity = +d.total_trade_activity
+  d.import_2018 = +d.import_2018
+  d.export_2018 = +d.export_2018
+  return d
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Old code for the barchart version
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var tradeVolumeDataset
+
+// d3.csv('./data/csv/allState2018.csv', function (error, csvdata) {
+  
+//   // Sort Data
+//   if (error) { 
+//     console.log('There was an error loading trade volume data: ', error) 
+//   } else {
+//     console.log('csvdata = ', csvdata)
+//     var dataset = d3.layout.stack()(["import_2018", "export_2018"].map(function(trade) {
+//       return csvdata.map(function(d) {
+//         return {x: parseInt(d.total_trade_activity), y: +d[trade]}
+//       })
+//     })
+//     )
+//     console.log('the new dataset = ', dataset)
+//     tradeVolumeDataset = dataset
+
+//     }
+  
+//   // Setup chart 
+//   var margin = {top: 50, right: 10, bottom: 0, left: 10}
+//   var width = 432 - margin.right - margin.left
+//   var height = 577 - margin.top - margin.bottom
+
+//   var svg = d3.select('#bar')
+//               .append('svg')
+//               .attr("width", width + margin.left + margin.right)
+//               .attr("height", height + margin.top + margin.bottom)
+//               .append("g")
+//               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+//   var tooltip = d3.select('#bar')
+//     .append('div')
+//     .attr('class', 'tooltip')
+//     .style('opacity', 0)
+
+//   // Set up scale
+//   var yScale = d3.scale.ordinal()
+//             .domain(tradeVolumeDataset[0].map(function(d) { return d.x; }))
+//             .rangeRoundBands([10, height-10], 0.05)
+
+//   var xScale = d3.scale.linear()
+//             .domain([0, d3.max(tradeVolumeDataset, function(d) {
+//                           return d3.max(d, function(d) { return d.y0 + d.y;
+//                         })
+//                       })
+//                     ])  
+//             .range([0, width])
+    
+//   var colors = ['#6b55d3', '#cf5cc5']
+
+//   // // Axis
+//   // var yAxis = d3.svg.axis()
+//   //               .scale(yScale)
+//   //               .orient('left')
+
+//   // svg.append('g')
+//   //     .attr('class', 'y axis')
+//   //     .call(yAxis)   
+
+//   // Create groups for each series, rects for each segment
+//   var groups = svg.selectAll("g")
+//                   .data(tradeVolumeDataset)
+//                   .enter()
+//                   .append('g')
+//                   .attr("class", "volume")
+//                   .style("fill", function(d,i) { return colors[i]})
+          
+//   var rect = groups.selectAll("rect")
+//                     .data(function(d) {return d})
+//                     .enter()
+//                     .append('rect')
+//                     .attr("y", function(d) {return yScale(d.x)})
+//                     .attr("x", function(d) { return xScale(d.y0)})
+//                     .attr("height", yScale.rangeBand())
+//                     .attr("width", function(d) { return xScale(d.y0 + d.y) - xScale(d.y0)})
+//                     .on('mouseover', function (d) {
+//                       tooltip.transition()
+//                         .duration(500)
+//                         .style('opacity', 0.9)
+                
+//                       format = d3.format(',')
+                
+//                       var tip = '$' + d.y + ' B'
+                
+//                       tooltip.html(tip)
+//                         .style('left', (d3.event.pageX) + 'px')
+//                         .style('top', (d3.event.pageY - 28) + 'px')
+//                     })
+//                     .on('mouseout', function (d) {
+//                       tooltip.transition()
+//                         .duration(500)
+//                         .style('opacity', 0)
+//                     })
+
+//   var labels = svg.selectAll('text')
+//                   .data(csvdata)
+//                   .enter()
+//                   .append('text')
+//                   .text(function (d, i) {
+//                     return csvdata[i].state
+//                   })
+//                   .attr('text-anchor', 'left')
+//                   .attr('y', (d, i) => 26 + i * 12)
+//                   .attr('x', 0)
+//                   .attr('fill', 'white')
+//                   .style('font-size', '8px')
+//                   .style('font-family', 'sans-serif')
+          
+// })
